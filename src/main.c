db38336d5ce52a5b9ba5c7d9a9eb3e0d3a7f0b1b
Stephen Bates <sbates@raithlin.com>
2017-05-17 10:20:12 -0600

    main.c: Add command line arguments.
    Use the libargconfig to add the command line variables to the
    program.

--- src/main.c
+++ src/main.c
@@ -14,75 +14,47 @@
  */
 
 #include <argconfig/argconfig.h>
+
 #include "version.h"
 
-struct nvme_dev;
-struct p2pmem_dev;
-
-int nvme_read_handler(const char *optarg, void *value_addr,
-		      const struct argconfig_options *opt)
-{
-	return 0;
-}
-
-int nvme_write_handler(const char *optarg, void *value_addr,
-		       const struct argconfig_options *opt)
-{
-	return 0;
-}
-
-int p2pmem_handler(const char *optarg, void *value_addr,
-		   const struct argconfig_options *opt)
-{
-	return 0;
-}
-
 const char *desc = "Perform p2pmem and NVMe CMB testing (ver=" VERSION ")";
+
 static struct {
-	struct nvme_dev *nvme_read;
-	struct nvme_dev *nvme_write;
-	struct p2pmem_dev *p2pmem;
-	unsigned reset_bytes;
-	unsigned refresh;
-	int duration;
+	int nvme_read_fd;
+	const char *nvme_read_filename;
+	int nvme_write_fd;
+	const char *nvme_write_filename;
+	int p2pmem_fd;
+	const char *p2pmem_filename;
+	size_t size;
+	size_t chunk;
 } cfg = {
-	.refresh  = 1,
-	.duration = -1,
+	.size  = 1048576,
+	.chunk = 1024,
 };
-
-#define NVME_READ_OPTION {"nvme-read", .cfg_type=CFG_CUSTOM,		\
-			.value_addr=&cfg.nvme_read,			\
-			.argument_type=required_positional,		\
-			.custom_handler=nvme_read_handler,		\
-			.complete="/dev/nvme*",				\
-			.help="NVMe device to read"}
-
-#define NVME_WRITE_OPTION {"nvme-write", .cfg_type=CFG_CUSTOM,		\
-			.value_addr=&cfg.nvme_write,			\
-			.argument_type=required_positional,		\
-			.custom_handler=nvme_write_handler,		\
-			.complete="/dev/nvme*",				\
-			.help="NVMe device to write"}
-
-#define P2PMEM_OPTION {"p2pmem", .cfg_type=CFG_CUSTOM,			\
-			.value_addr=&cfg.p2pmem,			\
-			.argument_type=required_positional,		\
-			.custom_handler=p2pmem_handler,			\
-			.complete="/dev/p2pmem*",			\
-			.help="p2pmem buffer to use"}
 
 int main(int argc, char **argv)
 {
 	const struct argconfig_options opts[] = {
-		NVME_READ_OPTION,
-		NVME_WRITE_OPTION,
-		P2PMEM_OPTION,
-		{"reset", 'r', "", CFG_NONE, &cfg.reset_bytes, no_argument,
-		 "reset byte counters"},
-		{"refresh", 'f', "", CFG_POSITIVE, &cfg.refresh, required_argument,
-		 "gui refresh period in seconds (default: 1 second)"},
-		{"duration", 'd', "", CFG_INT, &cfg.duration, required_argument,
-		 "gui duration in seconds (-1 forever)"},
+		{"nvme-read", .cfg_type=CFG_FD_RD,
+		 .value_addr=&cfg.nvme_read_fd,
+		 .argument_type=required_positional,
+		 .force_default="/dev/nvme0n1",
+		 .help="NVMe device to read"},
+		{"nvme-write", .cfg_type=CFG_FD_WR,
+		 .value_addr=&cfg.nvme_write_fd,
+		 .argument_type=required_positional,
+		 .force_default="/dev/nvme1n1",
+		 .help="NVMe device to write"},
+		{"p2pmem", .cfg_type=CFG_FD_WR,
+		 .value_addr=&cfg.p2pmem_fd,
+		 .argument_type=optional_positional,
+		 .force_default="/dev/p2pmem0",
+		 .help="p2pmem device to use as buffer"},
+		{"size", 's', "", CFG_SIZE, &cfg.size, required_argument,
+		 "total size of data transfer"},
+		{"chunk", 'c', "", CFG_SIZE, &cfg.chunk, required_argument,
+		 "size of each single transfer (note OS might not honor this)"},
 		{NULL}
 	};
 
