7d018b94b4c1d8c46004738dda5e41d05da0f850
Stephen Bates <sbates@raithlin.com>
2017-05-23 11:54:18 -0600

    main.c: Add checking
    Add the ability to write data to the drive, obtain a checksum and then
    check the data transfer completed sucessfully.

--- src/main.c
+++ src/main.c
@@ -14,8 +14,11 @@
  */
 
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <argconfig/argconfig.h>
@@ -31,12 +34,69 @@ static struct {
 	const char *nvme_write_filename;
 	int p2pmem_fd;
 	const char *p2pmem_filename;
-	size_t size;
-	size_t chunks;
+	unsigned check;
+	size_t   chunks;
+	long     page_size;
+	int      seed;
+	size_t   size;
+	int      write_parity;
+	int      read_parity;
 } cfg = {
-	.size   = 4096,
+	.check  = 0,
 	.chunks = 1024,
+	.seed   = -1,
+	.size   = 4096,
 };
+
+static int writedata()
+{
+	int *buffer;
+	ssize_t count;
+	int ret = 0;
+
+	if (posix_memalign((void **)&buffer, cfg.page_size, cfg.size*cfg.chunks))
+		return -1;
+
+	cfg.write_parity = 0;
+	for (size_t i=0; i<(cfg.size*cfg.chunks/sizeof(int)); i++) {
+		buffer[i] = rand();
+		cfg.write_parity ^= buffer[i];
+	}
+	count = write(cfg.nvme_read_fd, (void *)buffer, cfg.size*cfg.chunks);
+	if (count == -1){
+		ret = -1;
+		goto out;
+	}
+
+out:
+	free(buffer);
+	return ret;
+}
+
+static int readdata()
+{
+	int *buffer;
+	ssize_t count;
+	int ret = 0;
+
+	if (posix_memalign((void **)&buffer, cfg.page_size, cfg.size*cfg.chunks))
+		return -1;
+
+	count = read(cfg.nvme_write_fd, (void *)buffer, cfg.size*cfg.chunks);
+	if (count == -1) {
+		ret = -1;
+		goto out;
+	}
+
+	cfg.read_parity = 0;
+	for (size_t i=0; i<(cfg.size*cfg.chunks/sizeof(int)); i++)
+		cfg.read_parity ^= buffer[i];
+
+out:
+	free(buffer);
+	return ret;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -44,12 +104,12 @@ int main(int argc, char **argv)
 	ssize_t count;
 
 	const struct argconfig_options opts[] = {
-		{"nvme-read", .cfg_type=CFG_FD_RD_DIRECT,
+		{"nvme-read", .cfg_type=CFG_FD_RDWR_DIRECT,
 		 .value_addr=&cfg.nvme_read_fd,
 		 .argument_type=required_positional,
 		 .force_default="/dev/nvme0n1",
 		 .help="NVMe device to read"},
-		{"nvme-write", .cfg_type=CFG_FD_WR_DIRECT,
+		{"nvme-write", .cfg_type=CFG_FD_RDWR_DIRECT,
 		 .value_addr=&cfg.nvme_write_fd,
 		 .argument_type=required_positional,
 		 .force_default="/dev/nvme1n1",
@@ -59,10 +119,14 @@ int main(int argc, char **argv)
 		 .argument_type=required_positional,
 		 .force_default="/dev/p2pmem0",
 		 .help="p2pmem device to use as buffer"},
-		{"size", 's', "", CFG_SIZE, &cfg.size, required_argument,
-		 "size of data transfer"},
+		{"check", 'a', "", CFG_NONE, &cfg.check, no_argument,
+		 "perform checksum check on transfer (slow)"},
 		{"chunks", 'c', "", CFG_SIZE, &cfg.chunks, required_argument,
 		 "number of chunks to transfer"},
+		{"seed", 'b', "", CFG_INT, &cfg.seed, required_argument,
+		 "seed to use for random data (-1 for time based)"},
+		{"size", 's', "", CFG_SIZE, &cfg.size, required_argument,
+		 "size of data chunk"},
 		{NULL}
 	};
 
@@ -70,9 +134,14 @@ int main(int argc, char **argv)
 
 	p2pmem = mmap(NULL, cfg.size, PROT_READ | PROT_WRITE, MAP_SHARED,
 		      cfg.p2pmem_fd, 0);
-
 	if (p2pmem == MAP_FAILED)
 		perror("mmap");
+
+	if ( cfg.seed == -1 )
+		cfg.seed = time(NULL);
+	srand(cfg.seed);
+
+	cfg.page_size = sysconf(_SC_PAGESIZE);
 
 	fprintf(stdout,"Running p2pmem-test: Reading %s : Writing %s : "
 		"p2pmem Buffer %s.\n",cfg.nvme_read_filename, cfg.nvme_write_filename,
@@ -80,6 +149,16 @@ int main(int argc, char **argv)
 	fprintf(stdout,"\tchunk size = %zd : number of chunks =  %zd: total = %zd.\n",
 		cfg.size, cfg.chunks, cfg.size*cfg.chunks);
 	fprintf(stdout,"\tp2pmem = %p\n", p2pmem);
+	fprintf(stdout,"\tPAGE_SIZE = %ldB\n", cfg.page_size);
+	if (cfg.check)
+		fprintf(stdout,"\tchecking data with seed = %d\n", cfg.seed);
+
+	if (cfg.check)
+		if (writedata(cfg))
+			perror("writedata");
+
+	if (lseek(cfg.nvme_read_fd, 0, SEEK_SET) == -1)
+		perror("writedata-lseek");
 
 	for (size_t i=0; i<cfg.chunks; i++) {
 
@@ -93,6 +172,20 @@ int main(int argc, char **argv)
 		if (count == -1)
 			perror("write");
 	}
+
+	if (cfg.check) {
+		if (lseek(cfg.nvme_write_fd, 0, SEEK_SET) == -1)
+			perror("readdata-lseek");
+		if (readdata(cfg))
+			perror("readdata");
+	}
+
+	if (cfg.check)
+		fprintf(stdout, "%s on data check, 0x%x %s 0x%x.\n",
+			cfg.write_parity==cfg.read_parity ? "MATCH" : "MISMATCH",
+			cfg.write_parity,
+			cfg.write_parity==cfg.read_parity ? "=" : "!=",
+			cfg.read_parity);
 
 	munmap(p2pmem, cfg.size);
 
