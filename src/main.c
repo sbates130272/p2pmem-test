791334edd60f287ef46b85cce985f3b455e9089b
Stephen Bates <sbates@raithlin.com>
2017-05-31 06:19:32 -0600

    main.c: Add host_access mode.
    Add a mode whereby the host CPU accesses the p2pmem device directly
    using load/store accesses on the p2pmem device. If requested do both
    writes and reads and do a check to ensure the data read agrees with
    what was written.
    
    When --host_access is 0 no test is performed. When >0 then that value
    is used as the alignment and size of the access for both reads and
    writes. When <0 use -1*value for alignment and size but only do a read
    test. Use --check to perform a check on data (only when --host_access
    > 0).

--- src/main.c
+++ src/main.c
@@ -28,6 +28,8 @@
 
 #include "version.h"
 
+#define HOST_ACCESSES 32
+
 const char *desc = "Perform p2pmem and NVMe CMB testing (ver=" VERSION ")";
 
 static struct {
@@ -37,20 +39,95 @@ static struct {
 	const char *nvme_write_filename;
 	int p2pmem_fd;
 	const char *p2pmem_filename;
+	void     *buffer;
 	unsigned check;
 	size_t   chunk_size;
 	size_t   chunks;
+	int      host_access;
 	long     page_size;
+	int      read_parity;
 	int      seed;
 	size_t   size;
 	int      write_parity;
-	int      read_parity;
 } cfg = {
-	.check      = 0,
-	.chunk_size = 4096,
-	.chunks     = 1024,
-	.seed       = -1,
+	.check       = 0,
+	.chunk_size  = 4096,
+	.chunks      = 1024,
+	.host_access = 0,
+	.seed        = -1,
 };
+
+static int hosttest()
+{
+	unsigned char hostwrdata[abs(cfg.host_access)*HOST_ACCESSES],
+		hostrddata[abs(cfg.host_access)*HOST_ACCESSES];
+	size_t offset, offsets[HOST_ACCESSES],
+		hostbytes = abs(cfg.host_access)*HOST_ACCESSES;
+	struct hostaccess {
+		char entry[abs(cfg.host_access)];
+	};
+	struct hostaccess *hostdataptr = (struct hostaccess *)&hostwrdata[0];
+
+	if (hostbytes > cfg.size) {
+		errno = ENOMEM;
+		return -1;
+	}
+
+	if (cfg.host_access > 0) {
+
+		for (size_t i=0; i<hostbytes; i++)
+			hostwrdata[i] = (char)rand();
+
+		for(size_t i=0; i<HOST_ACCESSES; i++) {
+
+			offsets[i] = (size_t)(rand() % (cfg.chunk_size /
+							abs(cfg.host_access)));
+			*((struct hostaccess *)(cfg.buffer) + offsets[i]) =
+				*(hostdataptr + i);
+		}
+	}
+
+        hostdataptr = (struct hostaccess *)&hostrddata[0];
+	for(size_t i=0; i<HOST_ACCESSES; i++) {
+
+		if (cfg.host_access < 0 )
+			offset = (size_t)(rand() % (cfg.chunk_size /
+						    abs(cfg.host_access)));
+		else
+			offset = offsets[i];
+		*(hostdataptr + i) =
+			*((struct hostaccess *)(cfg.buffer) + offset);
+	}
+
+	if (cfg.check && (cfg.host_access > 0)) {
+
+		for (size_t i=0; i<HOST_ACCESSES; i++) {
+
+			unsigned compare = 1;
+			for (size_t j=0; j<HOST_ACCESSES; j++) {
+				if (offsets[i] == offsets[j]) {
+					compare = 0; // Don't check double writes.
+					break;
+				}
+			}
+			if (compare && (hostwrdata[i] != hostrddata[i])) {
+				fprintf(stdout, "MISMATCH on host_access %04zd "\
+					": 0x%02x != 0x%02x\n", i, hostwrdata[i],
+					hostrddata[i]);
+				errno = EINVAL;
+				return -1;
+			}
+		}
+		fprintf(stdout, "MATCH on host_access.\n");
+	}
+
+	/*
+	 * Reset RNG so writedata is consistent.
+	 */
+
+	srand(cfg.seed);
+	return 0;
+}
 
 static int writedata()
 {
@@ -104,7 +181,6 @@ out:
 
 int main(int argc, char **argv)
 {
-	void *buffer = NULL;
 	ssize_t count;
 	struct timeval start_time, end_time;
 	double val;
@@ -129,6 +205,8 @@ int main(int argc, char **argv)
 		 "perform checksum check on transfer (slow)"},
 		{"chunks", 'c', "", CFG_LONG_SUFFIX, &cfg.chunks, required_argument,
 		 "number of chunks to transfer"},
+		{"host_access", 'x', "", CFG_INT, &cfg.host_access, required_argument,
+		 "alignment and size for host access test (0 = no test, <0 = read only test)"},
 		{"seed", 'b', "", CFG_INT, &cfg.seed, required_argument,
 		 "seed to use for random data (-1 for time based)"},
 		{"size", 's', "", CFG_LONG_SUFFIX, &cfg.chunk_size, required_argument,
@@ -141,14 +219,14 @@ int main(int argc, char **argv)
 	cfg.size = cfg.chunk_size*cfg.chunks;
 
 	if (cfg.p2pmem_fd) {
-		buffer = mmap(NULL, cfg.chunk_size, PROT_READ | PROT_WRITE, MAP_SHARED,
-			      cfg.p2pmem_fd, 0);
-		if (buffer == MAP_FAILED) {
+		cfg.buffer = mmap(NULL, cfg.chunk_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+				  cfg.p2pmem_fd, 0);
+		if (cfg.buffer == MAP_FAILED) {
 			perror("mmap");
 			goto fail_out;
 		}
 	} else {
-		if (posix_memalign(&buffer, cfg.page_size, cfg.chunk_size)) {
+		if (posix_memalign(&cfg.buffer, cfg.page_size, cfg.chunk_size)) {
 			perror("posix_memalign");
 			goto fail_out;
 		}
@@ -165,11 +243,20 @@ int main(int argc, char **argv)
 	suf = suffix_si_get(&val);
 	fprintf(stdout,"\tchunk size = %zd : number of chunks =  %zd: total = %g%sB.\n",
 		cfg.chunk_size, cfg.chunks, val, suf);
-	fprintf(stdout,"\tbuffer = %p (%s)\n", buffer,
+	fprintf(stdout,"\tbuffer = %p (%s)\n", cfg.buffer,
 		cfg.p2pmem_fd ? "p2pmem" : "system memory");
 	fprintf(stdout,"\tPAGE_SIZE = %ldB\n", cfg.page_size);
+	if (cfg.host_access)
+		fprintf(stdout,"\tchecking host access %s: alignment and size = %dB\n",
+			cfg.host_access < 0 ? "(read only) " : "", abs(cfg.host_access));
 	if (cfg.check)
 		fprintf(stdout,"\tchecking data with seed = %d\n", cfg.seed);
+
+	if (cfg.host_access)
+		if (hosttest(cfg)) {
+			perror("hosttest");
+			goto free_fail_out;
+		}
 
 	if (cfg.check)
 		if (writedata(cfg)) {
@@ -185,14 +272,14 @@ int main(int argc, char **argv)
 	gettimeofday(&start_time, NULL);
 	for (size_t i=0; i<cfg.chunks; i++) {
 
-		count = read(cfg.nvme_read_fd, buffer, cfg.chunk_size);
+		count = read(cfg.nvme_read_fd, cfg.buffer, cfg.chunk_size);
 
 		if (count == -1) {
 			perror("read");
 			goto free_fail_out;
 		}
 
-		count = write(cfg.nvme_write_fd, buffer, cfg.chunk_size);
+		count = write(cfg.nvme_write_fd, cfg.buffer, cfg.chunk_size);
 
 		if (count == -1) {
 			perror("write");
@@ -224,18 +311,18 @@ int main(int argc, char **argv)
 	fprintf(stdout, "\n");
 
 	if (cfg.p2pmem_fd)
-		munmap(buffer, cfg.chunk_size);
+		munmap(cfg.buffer, cfg.chunk_size);
 	else
-		free(buffer);
+		free(cfg.buffer);
 
 	return EXIT_SUCCESS;
 
 
 free_fail_out:
 	if (cfg.p2pmem_fd)
-		munmap(buffer, cfg.chunk_size);
+		munmap(cfg.buffer, cfg.chunk_size);
 	else
-		free(buffer);
+		free(cfg.buffer);
 fail_out:
 	return EXIT_FAILURE;
 }
